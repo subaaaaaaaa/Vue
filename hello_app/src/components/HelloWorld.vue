c680502c6dceacc93c2389551c7308aa85b7506f
subaaaaaaaa <suba922.sk19@icloud.com>
2023-11-29 16:09:16 +0900

    vueの授業と練習問題

--- hello_app/src/components/HelloWorld.vue
+++ hello_app/src/components/HelloWorld.vue
@@ -2,6 +2,11 @@
   <div class="alert alert-info">
     <h2>{{ title }}</h2>
     <p>{{ message }}</p>
+    <hr>
+    <div>
+      <input type="text" class="form-control" v-model="input">
+      <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
+    </div>
   </div>
 </template>
 
@@ -9,8 +14,19 @@
 export default {
   name: 'HelloWorld',
   props: {
-    title:String,
+    title: String,
     message: String,
+  },
+  data() {
+    return {
+      message: 'お名前は？',
+      input: 'no name',
+    }
+  },
+  methods: {
+    doAction() {
+      this.message = 'こんにちは、' + this.input + 'さん！'
+    }
   }
 }
 </script>
---
+++ hello_app/src/components/Quiz.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="alert alert-primary">
+        <h2>クイズ作る</h2>
+        <p>{{ quiz_text }}</p>
+    </div>
+</template>
+<script>
+
+//お決まりでつけよう
+export default{
+    name:'Quiz',
+    props:{
+        quiz_text:String
+    }
+}
+</script>
